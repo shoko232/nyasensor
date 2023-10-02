51a741aae90006db0a75b6630aaa198c2935dc05
紫合　正子 <01042183@CA7671.local>
2023-10-02 12:14:50 +0900

    update json App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path={homeUrl} element={<Main />} />
+      <Route path={homeUrl + "/"} element={<Main />} />
       <Route path={homeUrl + "/list"} element={<List />} />
     </Routes>
     </>
